e746a4c0dfde00da91fabb11be9effa9d1b772f6
Brock Shaffer <contact@brockshaffer.dev>
2026-03-18 16:29:27 -0400

    improve UX of primary save

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Save } from "lucide-react"
+import { MapPin } from "lucide-react"
 import { Button } from "./components/ui/button"
 import { TopBar } from "./topbar"
 
@@ -6,8 +6,11 @@ export function App() {
   return (
     <div className="flex min-h-svh flex-col p-6">
       <TopBar />
-      <Button size="lg" className="mt-auto h-[33vh] w-full text-2xl">
-        <Save className="size-6" /> Save Location
+      <Button
+        size="lg"
+        className="mt-auto h-[33vh] w-full text-3xl active:scale-95"
+      >
+        <MapPin className="size-8" strokeWidth={2.5} /> Save Location
       </Button>
     </div>
   )
