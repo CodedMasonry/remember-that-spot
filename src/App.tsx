1e973131b58d466c53164dc64643a34279283008
Brock Shaffer <109301662+CodedMasonry@users.noreply.github.com>
2026-03-18 19:05:11 -0400

    create generic recent save cards

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,14 @@ export function App() {
   return (
     <div className="flex min-h-svh flex-col p-6">
       <TopBar />
-      <div className="mt-auto grid grid-cols-1 gap-6">
+      <div className="mt-auto grid grid-cols-1 gap-4">
         <RecentSave />
         <RecentSave />
         <RecentSave />
       </div>
       <Button
         size="lg"
-        className="mt-8 h-[33vh] w-full text-3xl active:scale-95"
+        className="mt-8 h-[33vh] w-full text-3xl font-semibold active:scale-[0.98]"
       >
         <MapPin className="size-8" strokeWidth={2.5} /> Save Location
       </Button>
--- src/components/recent-save.tsx
+++ src/components/recent-save.tsx
@@ -1,12 +1,74 @@
-import { Menu } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import {
+  ArrowDown,
+  ArrowDownLeft,
+  ArrowDownRight,
+  ArrowLeft,
+  ArrowRight,
+  ArrowUp,
+  ArrowUpLeft,
+  ArrowUpRight,
+  ChevronRight,
+  Compass,
+  Sunrise,
+  Sunset,
+} from "lucide-react"
+import { Separator } from "./ui/separator"
 
 export function RecentSave() {
   return (
     <div className="flex">
-      <Button size="lg" variant="outline">
-        Columbus, Ohio
+      <Button
+        size="lg"
+        variant="outline"
+        className="flex w-full flex-col items-start py-7 active:scale-[0.99]"
+      >
+        <div className="flex w-full items-center">
+          <p className="font-semibold">Football Stadium</p>
+          <p className="ml-auto font-light">2 hrs ago</p>
+          <ChevronRight className="ml-2" />
+        </div>
+        <div className="flex w-full items-center gap-4">
+          <p className="flex items-center font-light">
+            <HeadingIcon direction="NE" />
+            NE
+          </p>
+          <Separator orientation="vertical" />
+          <p className="font-light">1.3 mi away</p>
+          <Separator orientation="vertical" />
+          <p className="flex gap-1 font-light">
+            <Sunrise className="stroke-amber-500" /> 7:14
+          </p>
+          <p className="flex gap-1 font-light">
+            <Sunset className="stroke-amber-500" /> 18:27
+          </p>
+        </div>
       </Button>
     </div>
   )
+}
+
+type Direction = "N" | "NE" | "E" | "SE" | "S" | "SW" | "W" | "NW"
+
+function HeadingIcon({
+  direction,
+  className,
+}: {
+  direction: Direction
+  className?: string
+}) {
+  const icons: Record<string, React.ElementType> = {
+    N: ArrowUp,
+    NE: ArrowUpRight,
+    E: ArrowRight,
+    SE: ArrowDownRight,
+    S: ArrowDown,
+    SW: ArrowDownLeft,
+    W: ArrowLeft,
+    NW: ArrowUpLeft,
+  }
+
+  const IconComponent = icons[direction] || Compass
+
+  return <IconComponent stroke-width={2} className={className} />
 }
