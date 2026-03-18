66cf9825487b3ee3220f35c651bce6a29113c22a
Brock Shaffer <109301662+CodedMasonry@users.noreply.github.com>
2026-03-18 16:52:34 -0400

    create placeholder for cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { MapPin } from "lucide-react"
-import { Button } from "./components/ui/button"
-import { TopBar } from "./topbar"
+import { Button } from "@/components/ui/button"
+import { TopBar } from "@/components/topbar"
+import { RecentSave } from "./components/recent-save"
 
 export function App() {
   return (
     <div className="flex min-h-svh flex-col p-6">
       <TopBar />
+      <div className="mt-auto grid grid-cols-1 gap-6">
+        <RecentSave />
+        <RecentSave />
+        <RecentSave />
+      </div>
       <Button
         size="lg"
-        className="mt-auto h-[33vh] w-full text-3xl active:scale-95"
+        className="mt-8 h-[33vh] w-full text-3xl active:scale-95"
       >
         <MapPin className="size-8" strokeWidth={2.5} /> Save Location
       </Button>
---
+++ src/components/recent-save.tsx
@@ -0,0 +1,12 @@
+import { Menu } from "lucide-react"
+import { Button } from "@/components/ui/button"
+
+export function RecentSave() {
+  return (
+    <div className="flex">
+      <Button size="lg" variant="outline">
+        Columbus, Ohio
+      </Button>
+    </div>
+  )
+}
