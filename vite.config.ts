e1365b679cf90e6c14819228625ffc4ca718e479
Brock Shaffer <109301662+CodedMasonry@users.noreply.github.com>
2026-03-24 12:13:41 -0400

    add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,16 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import mkcert from "vite-plugin-mkcert"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), mkcert()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    mkcert(),
+    VitePWA({ registerType: "autoUpdate" }),
+  ],
   server: {
     https: {},
   },
